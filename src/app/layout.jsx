3320438e1731b7cd146cf3fb953eaabfa4801742
Muhammad Sony Setiawan <132990489+MuhammadSonySetiawan@users.noreply.github.com>
2023-12-12 01:24:38 +0700

    [UPDATE] font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
-import { Gabarito } from 'next/font/google'
+// import { Gabarito } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
-const gabarito = Gabarito({ subsets: ['latin'] })
+// const gabarito = Gabarito({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'AnimeList',
@@ -11,9 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${gabarito.className} bg-color-dark`}>
+      <body
+        // className={`
+        // // ${gabarito.className}
+        //  bg-color-dark`}
+        className="bg-color-dark"
+      >
         <div>
-          <Navbar/>
+          <Navbar />
         </div>
         {children}
       </body>
